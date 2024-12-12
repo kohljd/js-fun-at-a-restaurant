6eb384c8ab167f69b55ece4dbe4a644611cd47bf
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-12-12 17:25:33 -0600

    create menu item and recipe

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,42 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`;
 }
 
+function createMenuItem(foodName, price, type) {
+  return {
+    name: foodName,
+    price: price,
+    type: type,
+  }
+}
+
+function addIngredients(ingredient, ingredientList) {
+  if (!ingredientList.includes(ingredient)) {
+    ingredientList.push(ingredient);
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return price *= 0.9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
