d8ba7e29603ac9d56c9e2d66f79a8e2934171a35
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-12-12 18:11:24 -0600

    take, refund, list, and search orders

--- src/order.js
+++ src/order.js
@@ -1,7 +1,25 @@
+function takeOrder(order, orders) {
+  if (orders.length < 3) {
+    orders.push(order);
+  }
+}
+
+function refundOrder(orderNumber, orders) {
+  const orderIndex = orders.findIndex((order) => order.orderNumber === orderNumber);
+  orders.splice(orderIndex, 1);
+}
+
+function listItems(orders) {
+  return orders.map(order => order.item).join(", ");
+}
+
+function searchOrder(orders, item) {
+  return orders.some((order) => order.item === item);
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
