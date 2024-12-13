cf88e22366bdea18c15bd96266d653a4a3da14ab
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-12-12 20:22:18 -0600

    create restaurant, edit/check menu

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,46 @@
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+}
 
+function addMenuItem(restaurant, item) {
+  const menu = restaurant.menus[item.type];
+
+  if (!menu.includes(item)) {
+    menu.push(item);
+  }
+}
+
+function removeMenuItem(restaurant, item, menuType) {
+  const menu = restaurant.menus[menuType]
+  const itemIndex = menu.findIndex((food) => food.name === item);
+
+  if (itemIndex >= 0) {
+    menu.splice(itemIndex, 1);
+    return `No one is eating our ${item} - it has been removed from the ${menuType} menu!`;
+  }
+  return `Sorry, we don't sell ${item}, try adding a new recipe!`
+}
+
+function checkForFood(restaurant, item) {
+  const menu = restaurant.menus[item.type];
+
+  if (menu.includes(item)) {
+    return `Yes, we're serving ${item.name} today!`;
+  }
+
+  return `Sorry, we aren't serving ${item.name} today.`;
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem,
-  // checkForFood
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem,
+  checkForFood
 }
